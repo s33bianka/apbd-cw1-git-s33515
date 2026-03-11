bedf99f217493cdac72ac2a1819d5345969757dd
s33515 <s33515@pjwstk.edu.pl>
2026-03-11 13:11:13 +0100

    dodano metode calculateaverage

--- z2/z2/Program.cs
+++ z2/z2/Program.cs
@@ -3,3 +3,13 @@
 Console.WriteLine("first msg");
 
 Console.WriteLine("second msg");
+
+int CalculateAverage(int[] values)
+{
+    int fin = 0;
+    foreach (int value in values)
+    {
+        fin += value;
+    }
+    return fin / values.Length;
+}
